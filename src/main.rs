a0bdc7b12f23c2d3183abf11c5d4a91bcd151c86
julianuziemblo <julian.uziemblo@gmail.com>
2025-07-14 20:40:37 +0200

    first window

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::WindowResolution};
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn((Camera2d));
+}
+
+#[derive(Component)]
+struct Player {}
+
+fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((
+        Sprite::from_image(asset_server.load("placeholder.png")),
+        Player {},
+    ));
+}
 
 fn main() {
-    let mut app = App::new();
-    app.run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(800., 600.).with_scale_factor_override(4.),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_player)
+        .run();
 }
